59e7ba14662751d660693222390a69ff06fd7cce
Subir <y13uc295@lnmiit.ac.in>
2016-10-15 16:37:22 +0530

    Add files via upload
    Rating linkedin profile of user and sentiment analysis of his twitter tweets to get overall rough idea of how well balanced a person is towards his personal and professional life.

---
+++ Professional-and-Behavioural-Analysis/twitter.py
@@ -0,0 +1,49 @@
+import tweepy
+from tweepy import OAuthHandler
+ 
+consumer_key = 'DOg2nOm1L3krfmt0lYagtDh9U'
+consumer_secret = 'ZmTQxpl5c04sEOGBXMj8KkZ0FHGYkjpjSZIU15WJkhZFfc65gK'
+access_token = '1571934660-0fPRau14BOafEdaN7EWrhKd0Wj999lmWc763fUI'
+access_secret = 'mbaeVf5L4OEMddzH5qoHTmOLX9ztGAJVHagkbUC1CSfjG'
+ 
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+ 
+api = tweepy.API(auth)
+file = open("newfile.txt", "a")
+for tweet in tweepy.Cursor(api.user_timeline).items():
+	file.write(tweet.text)
+
+f = open("newfile.txt")
+data=f.read()
+from nltk.corpus import stopwords
+stop = set(stopwords.words('english'))
+words=[i for i in data.lower().split() if i not in stop]
+l=len(words)
+
+import numpy as np
+a=int(l/2)
+words1=words[0:a]
+words2=words[a+1:]
+data1=' '.join(words1)
+import re
+
+from itertools import chain
+text1 =re.sub(r"http\S+", "",data1).encode("utf-8")
+
+import requests
+payload1={"text":text1}
+r = requests.post("http://text-processing.com/api/sentiment/", data=payload1)
+x={}
+x=r.json()
+print(r.json)
+data2=' '.join(words2)
+import re
+
+from itertools import chain
+text2 =re.sub(r"http\S+", "",data2).encode("utf-8")
+
+import requests
+payload2={"text":text2}
+r2 = requests.post("http://text-processing.com/api/sentiment/", data=payload2)
+print(r2.content)
